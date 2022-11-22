358e1466d4e239d75a2939fc2930ae7655720a52
Imran Fazilov <ifazilov3584@bths.edu>
2022-11-22 12:19:03 -0500

    done

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String [] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a temperature in Fahrenheit: ");
+        int fahrenheit = scan.nextInt();
+        System.out.print("Enter a wind speed in mi/hr: ");
+        int windSpeed = scan.nextInt();
+
+        WindChill windChillIdx = new WindChill();
+        System.out.print(windChillIdx.WindChillEquation(fahrenheit, windSpeed));
+
+        for (int i = 5; i <= 60; i += 5) {
+            for (int j = 40; j >= -45; j -= 5) {
+                System.out.print(windChillIdx.WindChillEquation(j, i) + " ");
+            }
+            System.out.println();
+        }
+    }
 }
--- src/WindChill.java
+++ src/WindChill.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class WindChill {
+public class WindChill {
+    public WindChill() {
+
+    }
+
+    public int WindChillEquation (double fahrenheit, double windSpeed) {
+        return (int) (35.75 + (0.6215*fahrenheit) - (35.75*Math.pow(windSpeed, 0.16)) + (0.4275*fahrenheit*Math.pow(windSpeed, 0.16)));
+    }
 }
+
